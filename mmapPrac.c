ed5ecdcedb046ff88c5ab0c172a2f0cc16bb4600
Kyle <ko163@me.com>
2016-04-06 13:23:59 -0400

    Memory map reading file contents of multiple types of files

--- mmapPrac.c
+++ mmapPrac.c
@@ -8,13 +8,20 @@ int main(int argc, char const *argv[]) {
 	struct stat fileInfo;
 
 	// Opens File
-	fd = open("FS.txt", O_RDONLY);
+	fd = open("LICENSE", O_RDONLY);
 
 	// Gets file info and place into struct
-	printf("%d\n", fstat(fd, &fileInfo));
+	fstat(fd, &fileInfo);
 	printf("%d\n", fileInfo.st_size);
 	
-	//map = mmap(NULL, );
+	map = mmap(NULL, fileInfo.st_size, PROT_READ | PROT_WRITE,
+		MAP_PRIVATE, fd, 0);
+
+	for (i = 0; i < fileInfo.st_size; ++i)	{
+		printf("Entry: %d, Value: %c\n", i, map[i]);
+	}
+
+	munmap(map, fileInfo.st_size);
 
 	close(fd);
 
