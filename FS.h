ba33a85413e9d121b370c909728c50746e025116
Kyle <ko163@me.com>
2016-04-06 17:10:41 -0400

    Modified values tocheck for any scaling issues

--- FS.h
+++ FS.h
@@ -6,6 +6,11 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+typedef struct FATentry{
+	char next;
+	char block;
+} FATentry;
 
 #define NUM_OF_BLOCKS 4
 #define SIZE_OF_BLOCKS 10
+#define SIZE_OF_FAT NUM_OF_BLOCKS * sizeof(struct FATentry)
