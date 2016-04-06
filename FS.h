0c3027404e651c5ec73d7c8ea5759e6a219d9ecf
Kyle <ko163@me.com>
2016-04-06 01:57:13 -0400

    Added libraries for mmap and struct stat

--- FS.h
+++ FS.h
@@ -1,4 +1,11 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 
 #define NUM_OF_BLOCKS 4
-#define SIZE_OF_BLOCKS 16
+#define SIZE_OF_BLOCKS 10
