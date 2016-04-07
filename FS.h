a18a623277ddf92319ada2c7060dc9f431709b62
KyletheFox <ko163@me.com>
2016-04-07 16:35:09 -0400

    Added Specs about file headers

--- FS.h
+++ FS.h
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <unistd.h>
 
 typedef struct FATentry{
@@ -14,3 +15,20 @@ typedef struct FATentry{
 #define NUM_OF_BLOCKS 4
 #define SIZE_OF_BLOCKS 10
 #define SIZE_OF_FAT NUM_OF_BLOCKS * sizeof(struct FATentry)
+
+#define ROOT_BLOCK 0		// blocks[ROOT_BLOCK][x]
+
+
+
+/*	
+	Specs:
+
+	File Header:
+		Type 			4B
+		Name			12B
+		Last Access		8B	(ddmmyyyy)
+		Block Number    4B
+		Block Size 		4B
+------------------------------------------
+		Total: 			32B 
+*/
