c143cf0a4b95bb610c81311378511f4291af2332
KyletheFox <ko163@me.com>
2016-04-23 01:26:31 -0400

    testing for create, delete and open

--- some.c
+++ some.c
@@ -6,6 +6,9 @@ int main(int argc, char const *argv[])
 /*
 *	Objectinve: Get array of strings from pathname
 */
-	char path[] = "ROOT/monkey/apple/banana/pintree/laker/lcljdrhrrinvjkern";
-	koCreate(2, path);
+	char path[] = "ROOT/monkey/banana";
+	
+	//koCreate(2, path);
+	//koDelete(path);
+	printf("%d\n", koOpen(path) - SIZE_OF_FAT);
 }
