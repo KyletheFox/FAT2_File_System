a4bf029821524df68e506558e67eb5f6a8faa61f
KyletheFox <ko163@me.com>
2016-04-23 00:59:17 -0400

    Added koDelete func and when creating function, clear out any data that may have benn left over

--- diskFunc.c
+++ diskFunc.c
@@ -29,6 +29,7 @@ int koCreate(unsigned int type, char* pathname) {
 	int blockNum;			// The block number of the file to be created
 	int date;				// Holds the info about the current date
 	int index;				// The index of the block number rep by the file 
+	int i;					// Loop counter
 	struct FileHead newHead;	// The new file header to be inserted
 	struct tm *timeStamp;		// Holds data from the system timestamp
 	struct stat fileInfo;		// Holds metadata about the file to be opened
@@ -91,9 +92,17 @@ int koCreate(unsigned int type, char* pathname) {
 		if (writeHeader(newHead, map, index) != -1) {
 			// Update FAT table
 			updateFAT(blockNum, map, 0000);
-		}
 
-		return 1;
+			// Clear Block of any left over data
+			for (i = SIZE_OF_FAT + newHead.blockNum*SIZE_OF_BLOCKS; 
+					i < SIZE_OF_FAT + newHead.blockNum*SIZE_OF_BLOCKS + SIZE_OF_BLOCKS;
+					i++) {
+				printf("%d\n", i - SIZE_OF_FAT);
+				map[i] = 0;
+			}
+
+			return 1;
+		}
 	}
 
 	// File was not created
@@ -119,7 +128,79 @@ int koCreate(unsigned int type, char* pathname) {
 		- Update FAT
 */
 
+int koDelete(char *pathname) {
 
+	// Variables 
+	int fp; 					// File Pointer
+	int index;					// Holds location of file
+	int fileIndex = 0;			// Index of where the filename is in the parsedPath
+	int i = 0, j = 0;						// Loop counter
+	char **parsePath;			// Holds array of tokened pathname
+	char *map;					// memory map of the disk
+	char *fileName;			// Filename of the file to create
+	char *temp;					// Temporary Placeholder used to get last parsed path
+	struct stat fileInfo;		// Holds metadata about the file to be opened
+	struct FileHead tempHead;		// Place holder for file headers in data block
+
+
+	fp = open("FS.txt", O_RDWR);	// Opening disk
+
+	// Gets file info and place into struct
+	fstat(fp, &fileInfo);
+
+	// Memory Mapped File
+	map = (char*)mmap(NULL, fileInfo.st_size, PROT_READ | PROT_WRITE,
+		MAP_SHARED, fp, 0);
+
+	// Get parsed pathname
+	parsePath = parsePathName(pathname);
+
+	// loop to get last element of pointer array
+	do {
+		temp = parsePath[fileIndex];
+		printf("%d %s\n", fileIndex, temp);
+		if(temp == NULL) 
+			fileIndex--;
+		else
+			fileIndex++;
+	} while (temp != NULL);
+
+	// Get File Name
+	fileName = parsePath[fileIndex];
+
+	printf("%s\n", fileName);
+
+	// Remove Filename from parsePath to get index of block where file header is located
+	parsePath[fileIndex] = NULL;
+
+	// Gets index of the bloch where the file header to delete is 
+	index = findFile(parsePath, map);
+
+	while (i <= SIZE_OF_BLOCKS) {
+		tempHead = GetFileHead(map, index, &tempHead);
+		if(strcmp(convertFileName(fileName), tempHead.name) == 0) {
+			
+			// Clear the file header
+			for (j = index; j < SIZE_OF_FILEHEAD + index; ++j) {
+				map[j] = 0;
+			}
+
+			// Free Block Allocated by file header
+			updateFAT(tempHead.blockNum*2, map, 9999);
+
+			// Return Good 
+			return 1;
+		}
+		else {
+			i += SIZE_OF_FAT;
+			index += SIZE_OF_FAT;
+		}
+	}
+
+	// Return unsucessful
+	return -1;
+
+}
 
 /*
 	Close file
