c2b5cdb7c304f156fe26ac6334f6844cde965fe3
KyletheFox <ko163@me.com>
2016-04-24 01:38:26 -0400

    Added GetFATLink and alot of comments

--- FSFunctions.c
+++ FSFunctions.c
@@ -1,90 +1,113 @@
 #include "FS.h"
 
+// C++ command
 using namespace std;
 
+/*
+	Converts filenames so they are all the same size. All 
+	Files names are to be 12 chars long. If the given name
+	is too small, the front is padded in the front with '$'s.
+	If the file name is too long, the last n digits past the
+	12th are cut off.
+*/
 char *convertFileName(char *givenName) {
 
-	int length = strlen(givenName);
-	int i;
-	char *sizedName;
+	int length = strlen(givenName); // Gets the length of the name to convert
+	int i;							// Loop Counter
+	char *sizedName;				// Formatted Name to return
 
 	// Allocate memory in heap for new char *
 	sizedName = (char *)malloc(sizeof(char)*MAX_FILE_NAME_SIZE + 1);
 
+	// Loop that formats the file name
 	for (i = 0; i < MAX_FILE_NAME_SIZE; ++i) {
-		if (i < (MAX_FILE_NAME_SIZE - length))
+		
+		// Pads '$'s to the front of the name if it is smaller than 12
+		if (i < (MAX_FILE_NAME_SIZE - length)) {
 			sizedName[i] = '$';
-		else
+		}
+
+		// Places the given name into the formatted name
+		else {
 			sizedName[i] = givenName[i - (MAX_FILE_NAME_SIZE - length) ];
+		}
+
 	}
 
 	// Add eos
 	sizedName[MAX_FILE_NAME_SIZE] = '\0';
+
 	return sizedName; 
 
 }
 
-// Does not move the file pointer since using mmap)
+/*
+	Gets a File Header from the specified index. Goes through each char
+	of the FileHead and inputs them in to a FileHead struct to return
+*/
 struct FileHead GetFileHead(char *disk, int startIndex, struct FileHead *output) {
 
-	// Data Fields for FileHead struct
+	// Variables
+	char *name;					// File Header Name			
+	unsigned int i;				// Loop Counter
+	int index = startIndex;		// Index of File
+
+	// Init Data Fields for FileHead struct
 	output->type = 0;	
 	output->lastAccess = 0;
 	output->blockNum = 0;
 	output->size = 0;
-	char *name;								
-	
-	unsigned int i;						// Loop Counter
-	int index = startIndex;		// Index of File
 
-	name = (char*)malloc(13);
+	// Allocate data for file header name
+	name = (char*)malloc(MAX_FILE_NAME_SIZE + 1);
 
+	// Loop through the chars to insert into struct
 	for (i = 0; i < sizeof(struct FileHead); ++i) {
 
+		// Gets the first 4 chars - Type
 		if (i <= 3) {
 			output->type += disk[index];
 			if (i < 3)
 				output->type *= 10;				
 		}
 
+		// Gets 12 chars - Name
 		if (i >= 4 && i <= 15) 	{
 			name[i-4] = disk[index];
 		}
 
+		// Gets 8 chars - LastAcess
 		if (i >= 16 && i <= 23) {
 			output->lastAccess += disk[index];
 			if (i < 23)
 				output->lastAccess *= 10; 
 		}
 
+		// Gets 4 chars - blockNum
 		if (i >= 24 && i <= 27) {
 			output->blockNum += disk[index];
 			if (i < 27)
 				output->blockNum *= 10;
 		}
 
+		// Get 4 chars - Size
 		if (i >= 28 && i <= 31) {
 			output->size += disk[index];
 			if (i < 31)
 				output->size *= 10;
 		}
 
+		// Increase counter to read next char
 		index++;
 	}
 
+	// Add eos to name
 	name[MAX_FILE_NAME_SIZE] = '\0';
+
+	// Insert Name
 	output->name=name;
 
-	/*
-	printf("Type: %d\n", output->type);
-	printf("Name: %s\n", output->name);
-	printf("lastAccess: %d\n", output->lastAccess);
-	printf("blockNum: %d\n", output->blockNum);
-	printf("size: %d\n", output->size);
-	*/
-
 	return *output;
-
 }
 
 /*
@@ -155,6 +178,7 @@ int getNextFreeBlock(char *map) {
 	int freeBlock;		// Index of free block
 	int i;				// Loop Counter
 
+	// Loop through FAT table starting from the first one
 	for(i = 0; i < SIZE_OF_FAT; i+=2) {
 		if(map[i] == 99) {
 			freeBlock = i;
@@ -163,7 +187,6 @@ int getNextFreeBlock(char *map) {
 	}
 
 	return -1; 	// Free block not found
-
 }
 
 /*
@@ -180,13 +203,48 @@ void updateFAT(int fatIndex, char* map, int newValue) {
 
 	// Check if freeing block and if there is a link
 	if (first == 99 && second == 99 && map[fatIndex] != 00 && map[fatIndex + 1] != 00) {
-		inner = map[fatIndex] * 100;
-		inner += map[fatIndex + 1];
-		updateFAT(inner, map, 9999);
+		inner = map[fatIndex] * 100;	// Get the highest sig figs of link
+		inner += map[fatIndex + 1];		// Get the least sig figs of link
+		updateFAT(inner, map, 9999);	// Recursively call function to free next link
 	}
 
+	// Insert chars into FAT represent free block 
 	map[fatIndex] = first;					
 	map[fatIndex + 1] = second;
+}
+
+/*
+	This gets the FAT value of the block where the index is at. This
+	index must be inside on of the data block. To get the FAT entry,
+	thhe function will take the index subtract the SIZE_OF_FAT and 
+	then divided the result should be the data block number that the 
+	index is at. This number will tell me what FAT entry needs to be 
+	returned. The function will return the two char values as a single 
+	integer.
+*/
+int getFATLink(int index, char* map) {
+
+	// Variables
+	int FATvalue, i;
+
+	// Error check to make the index inputed is in a data block
+	if (index < SIZE_OF_FAT) {
+		printf("Index is not part of a data block\n");
+		return -1;
+	}
+
+	// Finds which FAT entry the data blocks belong too
+	index -= SIZE_OF_FAT;
+	index /= SIZE_OF_BLOCKS;
+
+	// location of the FAT table for the block
+	i = index*2;
+
+	// Converting two chars of FAT entry into single int
+	FATvalue = map[i] * 100;
+	FATvalue += map[i+1];
+
+	return FATvalue;
 }
 
 /*
@@ -200,34 +258,53 @@ int findFile(char **parsePath, char* map) {
 	int i=0, j=0;					// Loop Counters
 	struct FileHead temp; 			// Temporay placeholder to hold values of File Header
 
+	/* 
+		Loop the parsePath, looking for each dir/file inside. If it
+		finds the correct path it moves on to the next one. It does
+		this until it finds the last one. It then sets index to the
+		index of the data block that file header points to. 
+	*/
 	while (parsePath[i] != NULL && j < SIZE_OF_BLOCKS) {
+		
+		// Gets the file header
 		temp = GetFileHead(map, index, &temp);
-		//printf("%s\n", temp.name);
-		//printf("%s\n", convertFileName(parsePath[i]));
+		
+		// Checks to see if there is a match
 		if (strcmp(convertFileName(parsePath[i]), temp.name) == 0) {
-			printf("Found %s\n", temp.name);
+
+			// Sets index to where the file header points to 
 			index = temp.blockNum*SIZE_OF_BLOCKS + SIZE_OF_FAT;
+
+			// Increments counter to look at next item in parsePath
 			i++;
-		} else {
+		} 
+
+		// If not found, increment counter to look at next file header in block
+		else {
 			index += SIZE_OF_FILEHEAD;
-			j += SIZE_OF_FILEHEAD;
-			printf("Not Found: %s\n", convertFileName(parsePath[i]));
 		}
+	
 	}
 
+	// If last parsePath is not found
 	if (j >= SIZE_OF_BLOCKS) {
-		return -1;
+		return -1;	// Unsucessful
 	}
+
+	// Returns index if sucessful
 	else {
 		return index;
 	}
 }
 
+/*
+	Writes header to the data block located at the index
+*/
 int writeHeader(struct FileHead head, char* map, int index) {
 
 	int i=0, j=0; 			// Loop counter
-	struct FileHead temp;
-	int good = 0;
+	struct FileHead temp;	// Placeholder for FileHead entry
+	int good = 0;			// Flag to see if the file header was found
 
 	/* 
 		Used to print out characters starting from the front.
@@ -242,13 +319,24 @@ int writeHeader(struct FileHead head, char* map, int index) {
 
 	// Find empty space in data block to put header
 	while (j < SIZE_OF_BLOCKS && !good) {
+		
+		// Pulls File header from the disk file
 		temp = GetFileHead(map, index, &temp);
+		
+		// If the file header doesn't exist
 		if (temp.type != 0) {
-			index += SIZE_OF_FILEHEAD;
+
+			// Increase counters to where the next file header is
+			index += SIZE_OF_FILEHEAD;	
 			j += SIZE_OF_FILEHEAD;
+		
 		}
-		else
+		
+		// File was found	
+		else {
 			good = 1;		// True
+		}
+	
 	}
 
 	// print message is no space was found
@@ -256,6 +344,8 @@ int writeHeader(struct FileHead head, char* map, int index) {
 		printf("No More Space in Directory: File was NOT created\n");
 		return -1;
 	}
+
+	// Space was found
 	else {
 
 		// Write file header starting at the index found above
