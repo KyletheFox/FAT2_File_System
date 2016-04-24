69dce4df0e15df3594f372cb6e0798f6156dde87
KyletheFox <ko163@me.com>
2016-04-24 01:37:20 -0400

    Added test for koRead

--- some.c
+++ some.c
@@ -6,9 +6,10 @@ int main(int argc, char const *argv[])
 /*
 *	Objectinve: Get array of strings from pathname
 */
-	char path[] = "ROOT/monkey/banana";
+	char path[] = "ROOT/";
 	
 	//koCreate(2, path);
 	//koDelete(path);
-	printf("%d\n", koOpen(path) - SIZE_OF_FAT);
+	//printf("%d\n", koOpen(path) - SIZE_OF_FAT);
+	koRead(path);
 }
