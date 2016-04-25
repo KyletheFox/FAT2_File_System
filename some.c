04bae011f2d9d7b1c7bbab5cd325e77f5b9952f1
KyletheFox <ko163@me.com>
2016-04-24 21:16:59 -0400

    more testing.....

--- some.c
+++ some.c
@@ -6,10 +6,12 @@ int main(int argc, char const *argv[])
 /*
 *	Objectinve: Get array of strings from pathname
 */
-	char path[] = "ROOT/";
+	char path[] = "ROOT/monkey/ape";
+	char str[] = "kvskjvsbjkvdsajkbjkvadskbjkjbvdsbjkvdskbj vnjsnvjn fkvwklnvnklw wnvwnoiwv wovwivwfnjio";
 	
 	//koCreate(2, path);
 	//koDelete(path);
-	//printf("%d\n", koOpen(path) - SIZE_OF_FAT);
-	koRead(path);
+	printf("%d\n", koOpen(path) - SIZE_OF_FAT);
+	//printf("%s\n", koRead(path));
+	//koWrite(path, str);
 }
